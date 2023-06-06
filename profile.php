856663ba6b66e847cd7c974184986c71b49e0710
Aryasaputra <Aryasaputra@gmail>
2023-06-07 00:48:04 +0800

    commit pertama: menambahkan semua fail hasil praktikum 2

---
+++ profile.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/putra.jpg" alt="Arya Saputra" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2110010044</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Arya Saputra</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 21 Juli 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0812 5631 1605</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">AryaSaputra@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://m.facebook.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="https://instagram.com/putptrra._21?" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/" target="_blank">
+                                <img src="img/medsos/icons8-linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
